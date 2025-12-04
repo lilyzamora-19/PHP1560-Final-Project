3c3cb267d2f8bbcef771c43c5f0f9d48cc191f2a
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-04 18:38:10 -0500

    Update 01. Data.R

--- 01. Data.R
+++ 01. Data.R
@@ -1,8 +1,25 @@
-#'
-#'
-#'
+#'Load Data, Packages, and Clean Data
+
+#Packages
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
 
 #Load in Data
 soccer_data <- read.csv("/Users/lilyzamora/Desktop/PHP 1560/Data/CrowdstormingDataJuly1st.csv")
 
-#Cleaning Data
+#CLEAN DATA#
+
+#Take specific columns
+soccer_data <- soccer_data[, c("playerShort", "player", "position", "yellowCards", 
+                               "yellowReds", "redCards", "rater1", "rater2",
+                               "refNum", "meanIAT", "nIAT", "seIAT", "meanExp",
+                               "nExp", "seExp")]
+
+#Remove NAs
+soccer_data <- na.omit(soccer_data)
+
+#Remove small sample sizes (less than 100?)
+
+soccer_data <- subset(soccer_data, nIAT > 100)
+soccer_data <- subset(soccer_data, nExp > 100)
