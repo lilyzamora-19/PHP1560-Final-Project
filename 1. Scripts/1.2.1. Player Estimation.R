44fb7bf3f6c346888ae8ffb4bb613bfd592427f5
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-04 19:18:15 -0500

    Update 1.2.1. Player Estimation.R

--- 1. Scripts/1.2.1. Player Estimation.R
+++ 1. Scripts/1.2.1. Player Estimation.R
@@ -1,7 +1,8 @@
 #' Estimating Red Cards for Players on Skin Tones
 #' 
-#' @description
-#' @param1
+#' @description This function estimates the amount of red cards given to 
+#' players given their skin color.
+#' @param1 soccer_data A data frame with our cleaned data.
 #' @return 
 
 estimation_players <- function(soccer_data) {
