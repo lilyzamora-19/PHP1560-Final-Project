84ab2e494900a590ffde5838699494d8e4ceb80f
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-06 15:14:36 -0500

    Create 1.2.1. Player Estimation.R

---
+++ 1. Scripts/1.2. Estimations/1.2.1. Player Estimation.R
@@ -0,0 +1,23 @@
+#' Estimating Red Cards for Players on Skin Tones
+#' 
+#' @description This function estimates the amount of red cards given to 
+#' players given their skin color.
+#' @param1 soccer_data A data frame with our cleaned data.
+#' @return a data frame with an rate of red and yellow cards with position
+#' and skin color average rating.
+
+estimation_players <- function(soccer_data) {
+  players <- soccer_data %>%
+    group_by(player, position, avg_rating) %>%
+    summarise(
+      games = n(),
+      red_cards = sum(redCards, na.rm = TRUE),
+      yellow_cards = sum(yellowCards, na.rm = TRUE),
+      red_rate = redCards / games,
+      yellow_rate = yellowCards / games)
+  
+  return(players)
+}
+
+player_estimate <- estimation_players(soccer_data)
+
