9e399cd5975e1ae5fb0a147b2d878e52cd55fbc1
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-06 16:32:23 -0500

    Update 1.2.2. Referee Estimation.R

--- 1. Scripts/1.2. Estimations/1.2.2. Referee Estimation.R
+++ 1. Scripts/1.2. Estimations/1.2.2. Referee Estimation.R
@@ -1,5 +1,17 @@
 #'Estimating Red Card Probability of Referees
 #'
-#' @description
-#' @param1
+#' @description estimates the probability that the referee gives a red card 
+#' given the skin color of the players.
+#' @param soccer_data A data frame with our cleaned data.
+#' @return 
+
+estimation_referee <- function(soccer_data){
+  referee <- soccer_data$refNum
+  
+  soccer_data %>% 
+    
+    
+    
+  return(referee)
+}
 
