2d349a2447cdee239e2869a08c9738aee207c487
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-08 17:23:34 -0500

    Update 2.1. Simulation Tests.R

--- 2. Tests/2.1. Simulation Tests.R
+++ 2. Tests/2.1. Simulation Tests.R
@@ -1,10 +1,18 @@
 #' Simulation Tests
 
-#Run with different games
-sim_game(player_estimate, referee_estimate, games = 10)
+#Run with different amount of games to simulate since there it is more
+#likely for a team skin tone to be light or very light in comparison to
+#dark or very dark. The more samples we have, the more we can observe in our
+#results
 
-sim_game(player_estimate, referee_estimate, games = 50)
+test_10 <- sim_game(player_estimate, referee_estimate, games = 10)
 
-sim_game(player_estimate, referee_estimate, games = 100)
+test_50 <- sim_game(player_estimate, referee_estimate, games = 50)
 
-sim_game(player_estimate, referee_estimate, games = 500)
+test_100 <- sim_game(player_estimate, referee_estimate, games = 100)
+
+test_500 <- sim_game(player_estimate, referee_estimate, games = 500)
+
+test_1000 <- sim_game(player_estimate, referee_estimate, games = 1000)
+
+test_5000 <- sim_game(player_estimate, referee_estimate, games = 5000)
