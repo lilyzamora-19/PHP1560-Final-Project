f0b02cff0aa8ab28330ef0dba0671aca59508c4d
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-08 16:40:05 -0500

    Update 1.2.1. Player Estimation.R

--- 1. Scripts/1.2. Estimations/1.2.1. Player Estimation.R
+++ 1. Scripts/1.2. Estimations/1.2.1. Player Estimation.R
@@ -24,15 +24,15 @@ estimation_players <- function(soccer_data) {
 player_estimate <- estimation_players(soccer_data)
 
 # 
-estimation_players <- function(soccer_data) {
-  players <- soccer_data %>%
-    group_by(player, position) %>%
-    summarize(avg_rating = mean(avg_rating),
-              .groups = "drop") %>%
-    mutate(skin_tone = case_when(avg_rating >= 0 & avg_rating < 0.25 ~ "very_light",
-                                 avg_rating >= 0.25 & avg_rating < 0.50 ~ "light",
-                                 avg_rating >= 0.50 & avg_rating < 0.75 ~ "dark",
-                                 avg_rating >= 0.75 & avg_rating < 1 ~ "very_dark"))
-  return(players)
-}
-player_estimate <- estimation_players(soccer_data)
+#estimation_players <- function(soccer_data) {
+#  players <- soccer_data %>%
+#    group_by(player, position) %>%
+#    summarize(avg_rating = mean(avg_rating),
+#              .groups = "drop") %>%
+#    mutate(skin_tone = case_when(avg_rating >= 0 & avg_rating < 0.25 ~ "very_light",
+#                                 avg_rating >= 0.25 & avg_rating < 0.50 ~ "light",
+#                                 avg_rating >= 0.50 & avg_rating < 0.75 ~ "dark",
+#                                 avg_rating >= 0.75 & avg_rating < 1 ~ "very_dark"))
+#  return(players)
+#}
+#player_estimate <- estimation_players(soccer_data)
