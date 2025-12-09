c073bdba194468bc1686ea7c709518f2962ad294
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-08 19:34:44 -0500

    Update 3.1. Result Script.R

--- 3. Results/3.1. Result Script.R
+++ 3. Results/3.1. Result Script.R
@@ -1,20 +1,30 @@
 #' Results
-#' 
-#' @description
-#' @param
-#' @return
 
 #Utilizing a simulation of 5000 games
 simresults_5000 <- sim_game(player_estimate, referee_estimate, games = 5000, seed = 123)
 
 #Taking the data frame from the results, let's look deeper into the results.
 
+#Table
 table_gt <- tbl_summary(simresults_5000[,3:4], 
             by = team_skin_tone,
             label = list(total_cards ~ "Total Cards"),
             statistic = list(
               all_continuous() ~ "{median} ({mean}) {min} ({max})"))
+print(table_gt)
 
 kable_table <- as_kable(table_gt)
 print(kable_table) #Do this in R Markdown File if we want this table
+
+#Graphs
+ggplot(simresults_5000, aes(x = team_skin_tone, y = total_cards)) +
+  geom_col() +
+  labs(x = "Overall Team Skin Tone",
+       y = "Total Cards",
+       title = "Team Skin Tones vs. Total Cards Given 5000 Game Simulation") +
+  theme_minimal() +
+  
+
+#Deeper Questions
+#With this simulation, is there a relationship
 
