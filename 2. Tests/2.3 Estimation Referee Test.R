4f09d63361b1d0cf426935f8e6ee57c1eb11a457
graceqli <grace_q_li@brown.edu>
2025-12-08 22:06:45 -0500

    Update 2.3 Estimation Referee Test.R

--- 2. Tests/2.3 Estimation Referee Test.R
+++ 2. Tests/2.3 Estimation Referee Test.R
@@ -1,5 +1,5 @@
 # estimation_referee test
 #
-# 
+# testing that the referees in referee_estimate matches referees in soccer_data, output should be TRUE
 
-
+all(referee_estimate$refNum %in% unique(soccer_data$refNum))
