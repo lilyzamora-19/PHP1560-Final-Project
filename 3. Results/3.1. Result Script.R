be9d86c3b019729d48dc0cb820cc84753564103f
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-08 21:23:55 -0500

    Update 3.1. Result Script.R

--- 3. Results/3.1. Result Script.R
+++ 3. Results/3.1. Result Script.R
@@ -8,11 +8,17 @@ simresults_5000 <- sim_game(player_estimate, referee_estimate, games = 5000, see
 #------TABLES------#
 
 #Get GT Summary Table For Analysis 
-table_gt <- tbl_summary(simresults_5000[,3:4], 
-            by = team_skin_tone,
-            label = list(total_cards ~ "Total Cards"),
+table_gt <- tbl_summary(simresults_5000, 
+            label = list(total_cards ~ "Total Cards",
+                         any_cards_light ~ "Light Skin Tone Total Cards",
+                         any_cards_verylight ~ "Very Light Skin Tone Total Cards",
+                         any_cards_dark ~ "Dark Skin Tone Total Cards",
+                         any_cards_verydark ~ "Very Dark Skin Tone Total Cards",
+                         game_id ~ "Game ID",
+                         refNum ~ "Referee Number"),
             statistic = list(
-              all_continuous() ~ "{median} ({mean}) {min} ({max})"))
+              all_continuous() ~ "{median} ({mean}) {min} ({max})",
+              all_categorical() ~ "{n} ({p}%)"))
 print(table_gt)
 
 #Kable Table for R Markdown
@@ -31,6 +37,8 @@ ggplot(simresults_5000, aes(x = team_skin_tone, y = total_cards)) +
                               "very_dark" = "Very Dark", "very_light" = "Very Light")) +
   theme(plot.title = element_text(face = "bold", size = 15)) +
   theme_minimal() 
+
+
 
 
 #Deeper Questions
