79c076c839b188d437cbb6955f31729c2752aeff
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-08 20:05:36 -0500

    Create 0.2. Clean Data.R

---
+++ 0. Data/0.2. Clean Data.R
@@ -0,0 +1,64 @@
+#'Load Data, Packages, and Clean Data
+
+#Packages
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(knitr)
+
+
+#Load in Data
+soccer_data <- read.csv("/Users/lilyzamora/Desktop/PHP 1560/Data/CrowdstormingDataJuly1st.csv")
+
+#CLEAN DATA#
+
+#Take specific columns
+soccer_data <- soccer_data[, c("playerShort", "player", "position", "yellowCards", 
+                               "yellowReds", "redCards", "rater1", "rater2",
+                               "refNum", "meanIAT", "nIAT", "seIAT", "meanExp",
+                               "nExp", "seExp")]
+
+#Remove NAs
+soccer_data <- na.omit(soccer_data)
+
+#Remove small sample sizes (less than 100?)
+
+soccer_data <- subset(soccer_data, nIAT > 100)
+soccer_data <- subset(soccer_data, nExp > 100)
+
+#Average skin tone ratings between rater 1 and rater 2, then categorize
+soccer_data <- soccer_data %>%
+  mutate(avg_rating = (rater1 + rater2) / 2) %>%
+  mutate(skin_tone = case_when(avg_rating >= 0 & avg_rating < 0.25 ~ "very_light",
+                               avg_rating >= 0.25 & avg_rating < 0.50 ~ "light",
+                               avg_rating >= 0.50 & avg_rating < 0.75 ~ "dark",
+                               avg_rating >= 0.75 & avg_rating < 1 ~ "very_dark"))
+
+#Rename positions to fit a 4-2-3-1 Soccer formation
+#This formation consists of 4 defenders (two centerbacks and two fullbacks),
+#two defensive midfielders, three attacking midfielders, and one striker, along
+#with a goalie. Each team consists of 11 players with this formation and goalie.
+
+#Center Forward is another name for Striker,
+#Left and Right wingers are wide attacking midfielders, and so will be renamed such.
+#Left and Right fullback and Center back will be given defender to simplify
+#Left, Right, and Center Midfielders are offensive so are attacking to simplify.
+
+unique(soccer_data$position) #This tells us all unique positions to rename
+
+soccer_data <- soccer_data %>%
+  mutate(position = case_when(
+    position == "Center Forward" ~ "Striker",
+    position == "Left Fullback" ~ "Defender",
+    position == "Right Fullback" ~ "Defender",
+    position == "Left Midfielder" ~ "Attacking Midfielder",
+    position == "Right Midfielder" ~ "Attacking Midfielder",
+    position == "Center Midfielder" ~ "Attacking Midfielder",
+    position == "Left Winger" ~ "Attacking Midfielder",
+    position == "Right Winger" ~ "Attacking Midfielder",
+    position == "Goalkeeper" ~ "Goalie",
+    position == "Center Back" ~ "Defender",
+    position == "Defensive Midfielder" ~ "Defensive Midfielder",
+    position == "Attacking Midfielder" ~ "Attacking Midfielder"))
+
+unique(soccer_data$position) #Shows us 5 new position designations we made
