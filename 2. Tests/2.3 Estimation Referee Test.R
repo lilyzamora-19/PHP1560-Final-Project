945651b6970f488d7ef13727b3cfd9e3addcf94e
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-09 00:08:03 -0500

    Update 2.3 Estimation Referee Test.R

--- 2. Tests/2.3 Estimation Referee Test.R
+++ 2. Tests/2.3 Estimation Referee Test.R
@@ -1,5 +1,6 @@
 # estimation_referee test
 #
-# testing that the referees in referee_estimate matches referees in soccer_data, output should be TRUE
+# testing that the referees in referee_estimate matches referees in soccer_data,
+# output should be TRUE
 
 all(referee_estimate$refNum %in% unique(soccer_data$refNum))
