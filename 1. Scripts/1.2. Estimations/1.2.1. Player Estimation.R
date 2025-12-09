7c946cb023279bf28995c7766565842d956f9ce2
graceqli <grace_q_li@brown.edu>
2025-12-08 20:30:51 -0500

    Update 1.2.1. Player Estimation.R

--- 1. Scripts/1.2. Estimations/1.2.1. Player Estimation.R
+++ 1. Scripts/1.2. Estimations/1.2.1. Player Estimation.R
@@ -14,26 +14,13 @@ estimation_players <- function(soccer_data) {
       red_cards = sum(redCards, na.rm = TRUE),
       yellow_cards = sum(yellowCards, na.rm = TRUE),
       yellow_red_cards = sum(yellowReds, na.rm = TRUE),
-      red_rate = redCards / games,
-      yellow_rate = yellowCards / games,
-      yellow_red_rate = yellow_red_cards / games)
+      red_rate = red_cards / games,
+      yellow_rate = yellow_cards / games,
+      yellow_red_rate = yellow_red_cards / games,
+      .groups = "drop")
   
   return(players)
 }
 
 player_estimate <- estimation_players(soccer_data)
 player_estimate <- na.omit(player_estimate)
-
-# 
-#estimation_players <- function(soccer_data) {
-#  players <- soccer_data %>%
-#    group_by(player, position) %>%
-#    summarize(avg_rating = mean(avg_rating),
-#              .groups = "drop") %>%
-#    mutate(skin_tone = case_when(avg_rating >= 0 & avg_rating < 0.25 ~ "very_light",
-#                                 avg_rating >= 0.25 & avg_rating < 0.50 ~ "light",
-#                                 avg_rating >= 0.50 & avg_rating < 0.75 ~ "dark",
-#                                 avg_rating >= 0.75 & avg_rating < 1 ~ "very_dark"))
-#  return(players)
-#}
-#player_estimate <- estimation_players(soccer_data)
