b38dbc301326bfa76d6caadba1418af4f0951525
lilyzamora-19 <lily_zamora@brown.edu>
2025-12-09 00:31:52 -0500

    Update 2.2 Estimation Player Test.R

--- 2. Tests/2.2 Estimation Player Test.R
+++ 2. Tests/2.2 Estimation Player Test.R
@@ -3,4 +3,4 @@
 # Testing to make sure that there are no duplicate rows and that were correctly 
 # grouped, output should be FALSE
 
-any(duplicate(player_estimate[, c("player", "position", "skin_tone")]))
+any(duplicated(player_estimate[, c("player", "position", "skin_tone")]))
